4828430810303eef813695a0340fd27ba7a39520
Sankara <serge.maikere@gmail.com>
2018-01-24 15:30:00 +0100

    Attempt again for the last time the retour

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 import { TabsEnablor } from '../providers/custom/tabsEnablor';
+import { ContactProvider } from '../providers/contact/contact';
+import { ToastProvider } from '../providers/toast/toast';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
