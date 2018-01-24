e9318b20c9b8932c3aba7c759aee17d2bac3071c
Deba <aldealdo@gmail.com>
2018-01-24 15:03:29 +0100

    Little bug fixed

--- src/pages/mods/mods.ts
+++ src/pages/mods/mods.ts
@@ -1,4 +1,4 @@
-import { ViewChild, Component } from '@angular/core';
+import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 // import { ModEditorComponent } from '../../components/mod-editor/mod-editor';
 
@@ -8,13 +8,6 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'mods.html',
 })
 export class ModsPage {
-
-  @ViewChild('mod-editor')
-    // modEditor: ModEditorComponent;
-    // rightMenu: MenuRightComponent;
-    // leftMenu: MenuLeftComponent;
-    // toolBar: ToolBarComponent;
-
     constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
