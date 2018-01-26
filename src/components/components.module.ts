5e2a5069e0c9228073bcf0ee0fe5d8650c33d1de
Sankara <serge.maikere@gmail.com>
2018-01-26 08:20:21 +0100

    minor merge adjustments

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,34 +1,4 @@
 import { NgModule } from '@angular/core';
-<<<<<<< HEAD
-import { NavbarComponent } from './navbar/navbar';
-import { SidebarEditComponent } from './sidebar-edit/sidebar-edit';
-import { SidebarCreateComponent } from './sidebar-create/sidebar-create';
-import { ViewToolsComponent } from './view-tools/view-tools';
-import { ModEditorComponent } from './mod-editor/mod-editor';
-import { ProductsComponent } from './products/products';
-import { OrdersComponent } from './orders/orders';
-import { ContactFormComponent } from './contact-form/contact-form';
-
-@NgModule({
-	declarations: [NavbarComponent,
-    SidebarEditComponent,
-    SidebarCreateComponent,
-    ViewToolsComponent,
-    ModEditorComponent,
-    ProductsComponent,
-    OrdersComponent,
-    ContactFormComponent],
-	imports: [],
-	exports: [NavbarComponent,
-    SidebarEditComponent,
-    SidebarCreateComponent,
-    ViewToolsComponent,
-    ModEditorComponent,
-    ProductsComponent,
-    OrdersComponent,
-    ContactFormComponent]
-=======
-
 
 @NgModule({
 	declarations: [
@@ -36,7 +6,6 @@ import { ContactFormComponent } from './contact-form/contact-form';
 	imports: [],
 	exports: [
     ]
->>>>>>> e9318b20c9b8932c3aba7c759aee17d2bac3071c
 })
 
 export class ComponentsModule {}
