d3f0c352d705f855cd28eb0aaecc60ba85fd3009
Deba <aldealdo@gmail.com>
2018-02-13 16:38:19 +0100

    Separeted Selection object in other Object Called Nebuchadnezzar. This is my ship

--- src/threeAngular/the-matrix/the-matrix.ts
+++ src/threeAngular/the-matrix/the-matrix.ts
@@ -62,7 +62,6 @@ export class TheMatrix implements AfterViewInit {
    * Making The Matrix a better world!
    */
   public render() {
-    // console.log(this.transformControls);
     this.theArchitect.sceneHelpers.updateMatrixWorld();
     this.theArchitect.scene.updateMatrixWorld();
     this.renderer.render(this.theArchitect.scene, this.theArchitect.camera);
@@ -71,7 +70,7 @@ export class TheMatrix implements AfterViewInit {
 
   /**
    * Calculate Aspect Ratio of The Matrix. Avoids compression and streching of the scene.
-   */
+   */  
   public calculateAspectRatio(){
     this.canvas.style.width = "100%";
     this.canvas.style.height = "100%";
@@ -141,7 +140,7 @@ export class TheMatrix implements AfterViewInit {
    * Enable Moving inside The Matrix
    */
   public addOrbitControls() {
-    this.controls = new THREE.OrbitControls(this.theArchitect.camera, this.canvas);
+    this.controls = new THREE.OrbitControls(this.theArchitect.camera);
     this.controls.rotateSpeed = 1.0;
     this.controls.zoomSpeed = 1.2;
 
@@ -152,7 +151,7 @@ export class TheMatrix implements AfterViewInit {
   }
 
   /**
-   * Give us power to control objects in The Matrix
+   * Give us power to control objects in The Matrix 
    */
   public addTransformControls() {
     this.transformControls = new THREE.TransformControls(this.theArchitect.camera, this.canvas);
@@ -163,6 +162,7 @@ export class TheMatrix implements AfterViewInit {
 
     this.transformControls.addEventListener('change', () => {
       this.transformControls.update();
+      this.theArchitect.nebuchadnezzar.updateRedBox();
       this.render();
     });
 
@@ -199,3 +199,4 @@ export class TheMatrix implements AfterViewInit {
   }
 
 }
+  
